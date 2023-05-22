bcb18d922120dfefb17561a52d5757fdb5f99b8f
Mexx <mexx0174@gmail.com>
2023-05-22 16:39:38 +0500

    ренейм

--- src/main/java/ru/digitalmagicians/ebito/EbitoApplication.java
+++ src/main/java/ru/digitalmagicians/ebito/EbitoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiniAvitoApplication {
+public class EbitoApplication {
   public static void main(String[] args) {
-    SpringApplication.run(MiniAvitoApplication.class, args);
+    SpringApplication.run(EbitoApplication.class, args);
   }
 }
