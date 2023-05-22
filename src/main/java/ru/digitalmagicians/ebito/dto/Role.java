547d11bae898339afa34faa20f4e662164ea8e3e
Mexx <mexx0174@gmail.com>
2023-05-22 15:58:03 +0500

    1

---
+++ src/main/java/ru/digitalmagicians/ebito/dto/Role.java
@@ -0,0 +1,5 @@
+package ru.digitalmagicians.ebito.dto;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/ru/digitalmagicians/ebito/service/AuthService.java
@@ -0,0 +1,9 @@
+package ru.digitalmagicians.ebito.service;
+
+import ru.digitalmagicians.ebito.dto.RegisterReq;
+import ru.digitalmagicians.ebito.dto.Role;
+
+public interface AuthService {
+    boolean login(String userName, String password);
+    boolean register(RegisterReq registerReq, Role role);
+}
