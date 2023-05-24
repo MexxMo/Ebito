c138389c23c48fc644e24ab0cd64236e0b2d9253
Mexx <mexx0174@gmail.com>
2023-05-25 01:18:22 +0500

    Created CommentController and added comments in CommentDto

--- src/main/java/ru/digitalmagicians/ebito/dto/CommentDto.java
+++ src/main/java/ru/digitalmagicians/ebito/dto/CommentDto.java
@@ -4,10 +4,10 @@ import lombok.Data;
 
 @Data
 public class CommentDto {
-    private Integer pk;
-    private String text;
-    private Integer author;
-    private String authorImage;
-    private String authorFirstName;
-    private Integer createdAt;
+    private Integer pk; // id комментария
+    private String text; // текст комментария
+    private Integer author; // id автора комментария
+    private String authorImage; // ссылка на аватар автора комментария
+    private String authorFirstName; // имя создателя комментария
+    private Integer createdAt; // дата и время создания комментария в миллисекундах с 00:00:00 01.01.1970
 }
