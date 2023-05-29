ce115b3addc45e4b2be02bc8527f0361e53ce085
CatUnderGlue <warmonds1996@gmail.com>
2023-05-29 18:35:32 +0500

    Remove unused import

--- src/main/java/ru/digitalmagicians/ebito/Security/WebSecurityConfig.java
+++ src/main/java/ru/digitalmagicians/ebito/Security/WebSecurityConfig.java
@@ -2,8 +2,6 @@ package ru.digitalmagicians.ebito.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
