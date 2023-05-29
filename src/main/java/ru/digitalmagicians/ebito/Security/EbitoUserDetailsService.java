75b1a93cfe2dbba764fc04c41be400ddb0bc2446
CatUnderGlue <warmonds1996@gmail.com>
2023-05-29 18:31:39 +0500

    Set up spring-security

---
+++ src/main/java/ru/digitalmagicians/ebito/Security/EbitoUserDetailsService.java
@@ -0,0 +1,52 @@
+package ru.digitalmagicians.ebito.Security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.digitalmagicians.ebito.dto.RegisterReq;
+import ru.digitalmagicians.ebito.dto.Role;
+import ru.digitalmagicians.ebito.entity.User;
+import ru.digitalmagicians.ebito.exception.UserAlreadyExistException;
+import ru.digitalmagicians.ebito.exception.UserNotFoundException;
+import ru.digitalmagicians.ebito.repository.UserRepository;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class EbitoUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.getUserByEmailIgnoreCase(username);
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException();
+        }
+        User user = optionalUser.get();
+        return new EbitoUserDetails(user);
+    }
+
+    public void createUser(RegisterReq registerReq) {
+        if (userRepository.existsByEmailIgnoreCase(registerReq.getUsername())) {
+            throw new UserAlreadyExistException();
+        }
+        User user = new User();
+        user.setFirstName(registerReq.getFirstName());
+        user.setLastName(registerReq.getLastName());
+        user.setEmail(registerReq.getUsername());
+        user.setPassword(encoder.encode(registerReq.getPassword()));
+        user.setPhone(registerReq.getPhone());
+        user.setRole(Role.USER);
+
+        userRepository.save(user);
+        log.debug("User successfully created = {}", user);
+    }
+}
--- src/main/java/ru/digitalmagicians/ebito/service/impl/AuthServiceImpl.java
+++ src/main/java/ru/digitalmagicians/ebito/service/impl/AuthServiceImpl.java
@@ -1,47 +1,52 @@
 package ru.digitalmagicians.ebito.service.impl;
 
-import org.springframework.security.core.userdetails.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.digitalmagicians.ebito.Security.EbitoUserDetailsService;
 import ru.digitalmagicians.ebito.dto.RegisterReq;
 import ru.digitalmagicians.ebito.dto.Role;
+import ru.digitalmagicians.ebito.exception.UserAlreadyExistException;
+import ru.digitalmagicians.ebito.exception.UserNotFoundException;
 import ru.digitalmagicians.ebito.service.AuthService;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class AuthServiceImpl implements AuthService {
 
-  private final UserDetailsManager manager;
+  private final EbitoUserDetailsService manager;
 
   private final PasswordEncoder encoder;
 
-  public AuthServiceImpl(UserDetailsManager manager, PasswordEncoder passwordEncoder) {
-    this.manager = manager;
-    this.encoder = passwordEncoder;
-  }
-
   @Override
   public boolean login(String userName, String password) {
-    if (!manager.userExists(userName)) {
+    try {
+      UserDetails userDetails = manager.loadUserByUsername(userName);
+      return encoder.matches(password, userDetails.getPassword());
+    } catch (UserNotFoundException e) {
+      log.error(e.getMessage());
       return false;
     }
-    UserDetails userDetails = manager.loadUserByUsername(userName);
-    return encoder.matches(password, userDetails.getPassword());
   }
 
   @Override
   public boolean register(RegisterReq registerReq, Role role) {
-    if (manager.userExists(registerReq.getUsername())) {
-      return false;
+    if (registerReq.getUsername().isBlank() || registerReq.getFirstName().isBlank()
+            || registerReq.getLastName().isBlank() || registerReq.getPhone().isBlank()
+            || registerReq.getPassword().isBlank()) {
+      throw new IllegalArgumentException("All fields must be filled");
     }
-    manager.createUser(
-        User.builder()
-            .passwordEncoder(this.encoder::encode)
-            .password(registerReq.getPassword())
-            .username(registerReq.getUsername())
-            .roles(role.name())
-            .build());
+    try {
+      manager.createUser(registerReq);
+    } catch (UserAlreadyExistException e) {
+      log.error(e.getMessage());
+    }
+    log.info("User {} successfully created", registerReq.getUsername());
     return true;
   }
 }
