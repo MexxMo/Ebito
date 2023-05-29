58e0e139369ca73299d640a244ca67cbbc24c340
CatUnderGlue <warmonds1996@gmail.com>
2023-05-29 18:32:42 +0500

    Create rest exception handler and exceptions

---
+++ src/main/java/ru/digitalmagicians/ebito/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,30 @@
+package ru.digitalmagicians.ebito.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<String> handleUserAlreadyExistException(UserAlreadyExistException exception) {
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException exception) {
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+        return ResponseEntity
+                .badRequest()
+                .body(exception.getMessage());
+    }
+}
