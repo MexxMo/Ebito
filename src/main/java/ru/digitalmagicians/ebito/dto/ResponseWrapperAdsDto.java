45a5b48c36e9a294a105536e9cb0c83310ca8c04
Maksim Syrov <warmonds1996@gmail.com>
2023-06-01 19:55:07 +0500

    Revert "Revert "Feature catunderglue""

--- src/main/java/ru/digitalmagicians/ebito/dto/ResponseWrapperAdsDto.java
+++ src/main/java/ru/digitalmagicians/ebito/dto/ResponseWrapperAdsDto.java
@@ -8,4 +8,10 @@ import java.util.List;
 public class ResponseWrapperAdsDto {
     private Integer count;
     private List<AdsDto> results;
+
+    public ResponseWrapperAdsDto(Integer count, List<AdsDto> results) {
+        this.count = count;
+        this.results = results;
+    }
+
 }
