4db6347bb250101fce57e23385d9cb54357504aa
Ilyas <xxxilyas92xxx@gmail.com>
2023-06-04 14:41:05 +0500

    fix bags ads serviceImpl

--- src/main/java/ru/digitalmagicians/ebito/service/impl/AdsServiceImpl.java
+++ src/main/java/ru/digitalmagicians/ebito/service/impl/AdsServiceImpl.java
@@ -42,7 +42,7 @@ public class AdsServiceImpl implements AdsService {
         ads.setPrice(properties.getPrice());
         ads.setAuthor(userService.getUserByEmail(authentication.getName()));
         Ads updatedAds = adsRepository.save(ads);
-//        log.info("Successful save ads: {}", ads);
+       log.info("Successful save ads");
         return adsMapper.toDto(updatedAds);
     }
 
