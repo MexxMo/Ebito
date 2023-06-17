917b7054daf7079017f82904b351d2ebb2291b95
Ilyas <xxxilyas92xxx@gmail.com>
2023-06-18 00:24:13 +0500

    Исправил удаление объявлений, если есть комментарии от других пользователей

--- src/main/java/ru/digitalmagicians/ebito/service/impl/AdsServiceImpl.java
+++ src/main/java/ru/digitalmagicians/ebito/service/impl/AdsServiceImpl.java
@@ -15,6 +15,7 @@ import ru.digitalmagicians.ebito.entity.Image;
 import ru.digitalmagicians.ebito.exception.AdsValidationException;
 import ru.digitalmagicians.ebito.mapper.AdsMapper;
 import ru.digitalmagicians.ebito.repository.AdsRepository;
+import ru.digitalmagicians.ebito.repository.CommentRepository;
 import ru.digitalmagicians.ebito.security.AccessChecker;
 import ru.digitalmagicians.ebito.service.AdsService;
 import ru.digitalmagicians.ebito.service.ImageService;
@@ -34,6 +35,7 @@ public class AdsServiceImpl implements AdsService {
     private final AdsMapper adsMapper;
     private final ImageService adsImageService;
     private final AccessChecker accessChecker;
+    private final CommentRepository commentRepository;
 
     @Override
     public AdsDto createAds(MultipartFile image, CreateAdsDto properties, Authentication authentication) {
@@ -125,6 +127,8 @@ public class AdsServiceImpl implements AdsService {
     public void delete(Integer id) {
         Ads ads = getAdsById(id);
         if (accessChecker.checkAccess(ads)) {
+            adsImageService.deleteImageFail(ads.getImage());
+            commentRepository.deleteAllByAds_Id(ads.getId());
             adsImageService.deleteImageFail(ads.getImage());
             adsRepository.delete(ads);
             log.info("Successful deleting ads by id: {}", id);
